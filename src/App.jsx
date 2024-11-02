3e6bb15cc69f4be1ff6f2d4b1196c1be6363c198
Ahmad M <ahmadkhaled.dev@gmail.com>
2024-11-02 17:47:39 +0200

    Fetch Posts From The API On Component Mount And Update State Then Inject Posts Into JSX

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,32 @@ import './App.css'
 import AuthModal from './Componants/AuthModal';
 import MainPosts from './Componants/MainPosts';
 import TopNav from './Componants/TopNav';
+import axios from 'axios';
 
 export default function App() {
+  const [postList, setPostList] = useState([])
+
+  // Effect to fetch posts from the API when the component mounts
+  useEffect(() => {
+    axios.get('https://tarmeezAcademy.com/api/v1/posts')
+      .then((res) => {
+        const fetchPosts = res.data.data
+        setPostList(fetchPosts)
+      })
+  }, [])
+
+  // Mapping over the postList to render MainPosts for each post
+  const renderedPosts = postList.map((post, index) => {
+    return <MainPosts key={index} />
+  })
+
 
   return (
     <>
       <TopNav />
 
       <MainPosts />
-      <MainPosts />
-      <MainPosts />
-
+      {renderedPosts}
 
     </>
   );
